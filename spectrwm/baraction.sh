af4088bc61bc35d63dcd0e089c127801b6794c5f
Hurshal Patel <hpatel516@gmail.com>
2013-10-25 18:24:05 -0700

    update spectrwm files

--- spectrwm/baraction.sh
+++ spectrwm/baraction.sh
@@ -2,27 +2,110 @@
 # baraction.sh for spectrwm status bar
 # From http://wiki.archlinux.org/index.php/Scrotwm
 
-bar() {
-    /home/hp/bin/bar "$@"
+split() {
+    BC="scale=0;a=$1/($2/$3);print a"
+    echo `echo $BC | bc -l`
 }
 
-SLEEP_SEC=5
-#loops forever outputting a line every SLEEP_SEC secs
-while :; do
+icon() {
+    echo "^i($ICONS/$1.xbm)"
+}
 
-  POWER_PERCENT=$(acpi | awk '/Battery 0: / { print $4 }' | sed 's/,//g')
-  POWER_NUM=$(echo $POWER_PERCENT | sed 's/%//g')
-  BCSCRIPT="scale=0;a=$POWER_NUM/10;print a"
-  BAR_NUM=`echo $BCSCRIPT | bc -l`
-  BCSCRIPT="a=10-$BAR_NUM;print a"
-  EMPTY_NUM=`echo $BCSCRIPT | bc -l`
-  POWER_STR="$(bar $BAR_NUM $EMPTY_NUM) $POWER_PERCENT"
+color() {
+    echo "^fg($1)"
+}
 
-  WLAN_STR=$(iwconfig wlan0 | awk -F "\"" '/wlan0/ { print $2 }')
+brightness() {
+  BACKLIGHT=$(xbacklight -get)
+  BRIGHTNESS=$(printf "%.0f" $BACKLIGHT)
+  COLOR=$FG
+  echo "$(color $FG)$(icon sun) $(color $GY)$BRIGHTNESS%"
+}
 
-  DATE_STR=$(TZ='America/Los_Angeles' date)
+wifi() {
+    WLAN=$(iwconfig wlan0 | awk -F "\"" '/wlan0/ { print $2 }')
+    echo "$(color $FG)$(icon wifi) $(color $GY)$WLAN"
+}
 
-  echo -E "    $WLAN_STR    $POWER_STR    $DATE_STR"
+power() {
+    POWER=$(acpi | awk '/Battery 0: / { print $4 }' | sed 's/%//g' | sed 's/,//g')
+    STATUS=$(acpi | awk '/Battery 0: / { print $3 }' | sed 's/,//g')
+    ICON=''
+    COLOR=$FG
+    case $STATUS in
+    'Discharging')
+        case `split $POWER 100 4` in
+        0)
+            ICON=$(icon battery-empty)
+            COLOR=$RD
+            ;;
+        1)
+            ICON=$(icon battery-critical)
+            COLOR=$OG
+            ;;
+        2)
+            ICON=$(icon battery-low)
+            ;;
+        *)
+            ICON=$(icon battery-full)
+            ;;
+        esac;
+        ;;
+    *)
+        case $POWER in
+        100)
+            ICON=$(icon ac-charged)
+            ;;
+        *)
+            ICON=$(icon ac-charging)
+            ;;
+        esac;
+        ;;
+    esac;
+    echo "$(color $COLOR)$ICON $(color $GY)$POWER%"
+}
 
-  sleep $SLEEP_SEC
-done
+volume() {
+    LINE=$(amixer get Master | sed -n '5p')
+    VOLUME=$(echo $LINE | awk '/Playback/ { print $4 }' | sed 's/\[\|\]//g')
+    STATUS=$(echo $LINE | awk '/Playback/ { print $6 }')
+    ICON=''
+    COLOR=''
+    case $STATUS in
+    '[on]')
+        ICON=$(icon spkr-on)
+        COLOR=$FG
+        ;;
+    '[off]')
+        ICON=$(icon spkr-off)
+        COLOR=$GY
+        ;;
+    esac;
+    echo "$(color $COLOR)$ICON $(color $GY)$VOLUME"
+}
+
+datetime() {
+    DATETIME=$(TZ='America/Los_Angeles' date | tr -s ' ' | cut -d\  -f1,2,3,4)
+    echo "$(color $FG)$(icon clock) $(color $GY)$DATETIME"
+}
+
+
+SLEEP_SEC=1
+FG=`cat /home/hp/.config/colorscheme/fg`
+BG='#0F0F0F'
+GY='#839496'
+RD='#d22626'
+OG='#b58900'
+FONT='-*-terminus-*-r-normal-*-*-100-*-*-*-*-iso8859-*'
+ICONS='/home/hp/.config/dzen2'
+HEIGHT='16'
+
+echo -e " $(icon arch) $(color $GY)arch$(color $FG)linux" | dzen2 -p -e - -h $HEIGHT -w '100' -ta l -fg $FG -bg $BG -fn $FONT &
+
+##loops forever outputting a line every SLEEP_SEC secs
+##while :; do
+##  #FG=`cat /home/hp/.config/colorscheme/fg`
+##  echo -E "$(brightness)    $(wifi)    $(volume)    $(power)    $(datetime) "
+##
+##  sleep $SLEEP_SEC
+##done | dzen2 -x 800 -e - -h $HEIGHT -w '566' -ta r -fg $FG -bg $BG -fn $FONT &
