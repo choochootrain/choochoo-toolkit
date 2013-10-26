e22c36c7d7b6627a9528422fd0962a40c4b7fe08
Hurshal Patel <hpatel516@gmail.com>
2013-10-25 18:50:45 -0700

    add setup script for x11

---
+++ x11/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+ln -sf `pwd`/xbindkeysrc ~/.xbindkeysrc
+ln -sf `pwd`/xinitrc ~/.xinitrc
+ln -sf `pwd`/Xmodmap ~/.Xmodmap
+ln -sf `pwd`/xprofile ~/.xprofile
+ln -sf `pwd`/Xresources ~/.Xresources
