3c7d65ce8602bcce0c0189a39d2774a5f18313c3
Hurshal Patel <hpatel516@gmail.com>
2013-10-25 18:30:06 -0700

    update spectrwm setup script

--- spectrwm/setup.sh
+++ spectrwm/setup.sh
@@ -2,3 +2,5 @@
 ln -sf `pwd`/spectrwm.conf ~/.spectrwm.conf
 ln -sf `pwd`/baraction.sh ~/bin/baraction.sh
 ln -sf `pwd`/screenshot.sh ~/bin/screenshot.sh
+ln -sf `pwd`/brightness.sh ~/bin/brightness.sh
+ln -sf `pwd`/volume.sh ~/bin/volume.sh
