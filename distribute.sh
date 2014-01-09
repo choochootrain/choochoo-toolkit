ca3eebe07cf388553a93ad42ba0d02d57a10f222
Hurshal Patel <hpatel516@gmail.com>
2014-01-08 16:26:29 -0800

    add interactive distribution script

--- distribute.sh
+++ distribute.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
+echo -e "#\n#Edit list of configs to be installed below:\n#" > .install
 FILES=$(ls -d */ | grep -v '\.git' | grep -v '_.*') 
 for dir in $FILES; do
+  echo $dir >> .install
+done
+
+$EDITOR .install
+
+for dir in $(cat .install | grep -v '#'); do
   cd $dir
   echo "Updating $dir configuration"
   ./setup.sh
   cd ..
 done
+
+rm .install
