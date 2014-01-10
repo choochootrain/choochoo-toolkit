6b6f931e9e2e5b0cd8ac14f88ced7c3d46dd2a46
Hurshal Patel <hpatel516@gmail.com>
2014-01-09 16:46:34 -0800

    add xrandr script for monitors

---
+++ x11/monitor.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+
+monitor() {
+  case $1 in
+  hdmi)
+    xrandr --output LVDS1 --auto --output HDMI1 --auto --right-of LVDS1
+    ;;
+  vga)
+    xrandr --output LVDS1 --auto --output VGA1 --auto --right-of LVDS1
+    ;;
+  off)
+    xrandr --output VGA1 --off
+    xrandr --output HDMI1 --off
+    ;;
+  *)
+    ;;
+  esac;
+}
+
+monitor $1
--- x11/setup.sh
+++ x11/setup.sh
@@ -3,3 +3,4 @@ ln -sf `pwd`/xinitrc ~/.xinitrc
 ln -sf `pwd`/Xmodmap ~/.Xmodmap
 ln -sf `pwd`/xprofile ~/.xprofile
 ln -sf `pwd`/Xresources ~/.Xresources
+ln -sf `pwd`/monitor.sh ~/bin/monitor.sh
