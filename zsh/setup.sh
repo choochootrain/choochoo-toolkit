ce94bb5aa0d866173a549e94b44ae096878215d8
Hurshal Patel <hpatel516@gmail.com>
2014-01-11 08:00:19 -0800

    split zshrc into alias and env files

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-ln -sf `pwd`/zshrc ~/.zshrc
-ln -sf `pwd`/zprofile ~/.zprofile
 ln -sf `pwd`/dir_colors ~/.dir_colors
 ln -sf `pwd`/findup ~/bin/findup
+ln -sf `pwd`/zshrc ~/.zshrc
+ln -sf `pwd`/zaliases ~/.zaliases
+ln -sf `pwd`/zshenv ~/.zshenv
