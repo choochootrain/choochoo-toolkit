5f0b359f3dfff2219879bdc6c531f532732f7154
Hurshal Patel <hpatel516@gmail.com>
2014-01-17 06:03:58 -0800

    clean up x11 configs

--- x11/setup.sh
+++ x11/setup.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 ln -sf `pwd`/xinitrc ~/.xinitrc
 ln -sf `pwd`/Xmodmap ~/.Xmodmap
-ln -sf `pwd`/xprofile ~/.xprofile
 ln -sf `pwd`/Xresources ~/.Xresources
 ln -sf `pwd`/monitor.sh ~/bin/monitor.sh
