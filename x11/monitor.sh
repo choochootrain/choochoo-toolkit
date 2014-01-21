426497842683f1dbe82e514bdd9c519f7c7b6f66
Hurshal Patel <hpatel516@gmail.com>
2014-01-21 12:54:24 -0800

    fix monitor script for vga

--- x11/monitor.sh
+++ x11/monitor.sh
@@ -7,7 +7,7 @@ monitor() {
     xrandr --output LVDS1 --auto --output HDMI1 --auto --right-of LVDS1
     ;;
   vga)
-    xrandr --output LVDS1 --auto --output VGA1 --auto --right-of LVDS1
+    xrandr --output LVDS1 --auto --output VGA1 --auto --left-of LVDS1
     ;;
   off)
     xrandr --output VGA1 --off
