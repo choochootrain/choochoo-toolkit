eb0e80d7d38ae8d922b2baad16ce018e08bb31f8
Hurshal Patel <hpatel516@gmail.com>
2014-03-26 19:46:45 -0700

    ditch jumanji for vimperator

--- jumanji/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-ln -sf `pwd`/jumanjirc ~/.config/jumanji/jumanjirc
-ln -sf `pwd`/adblock/easylist.txt ~/.config/jumanji/adblock/easylist.txt
-ln -sf `pwd`/adblock/easyprivacy.txt ~/.config/jumanji/adblock/easyprivacy.txt
-ln -sf `pwd`/scripts/hints.js ~/.config/jumanji/scripts/hints.js
