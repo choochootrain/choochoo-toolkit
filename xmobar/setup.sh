cb8e106326b946e2898b12d0f0539ff5dd5fdef5
Hurshal Patel <hpatel516@gmail.com>
2016-02-08 01:43:04 -0800

    [xmonad][xmobar] :put_litter_in_its_place:

--- xmobar/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-ln -sf `pwd`/xmobarrc ~/.xmobarrc
-ln -sf `pwd`/resources ~/.xmobar
-# TODO fix symlink loop
-rm resources/resources
--- xmonad/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-ln -sf `pwd`/volume.sh ~/bin/volume.sh
-ln -sf `pwd`/pop.wav ~/.config/sounds/pop.wav
-ln -sf `pwd`/xmonad.hs ~/.xmonad/xmonad.hs
-xmonad --recompile
