12d624a1eec36193819459cd5144df9a254c8c03
Hurshal Patel <hurshal@imap.cc>
2024-07-25 12:01:25 -0700

    fix yabai iterm binding

--- mac-scripts/open_iterm2.sh
+++ mac-scripts/open_iterm2.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-# https://github.com/Julian-Heng/chunkwm-config/blob/master/chunkwm/scripts/misc/open_iTerm2.sh
+# https://github.com/julian-heng/yabai-config/blob/master/scripts/open_iterm2.sh
 # Detects if iTerm2 is running
-if ! pgrep -f "iTerm" > /dev/null; then
-    open "/Applications/iTerm.app"
+if ! pgrep -f "iTerm" > /dev/null 2>&1; then
+    open -a "/Applications/iTerm.app"
 else
     # Create a new window
-    if ! osascript -e 'tell application "iTerm2" to create window with default profile' > /dev/null; then
+    script='tell application "iTerm2" to create window with default profile'
+    ! osascript -e "${script}" > /dev/null 2>&1 && {
         # Get pids for any app with "iTerm" and kill
-        for i in $(pgrep -f "iTerm"); do kill -15 "$i"; done
-        open "/Applications/iTerm.app"
-    fi
+        while IFS="" read -r pid; do
+            kill -15 "${pid}"
+        done < <(pgrep -f "iTerm")
+        open -a "/Applications/iTerm.app"
+    }
 fi
